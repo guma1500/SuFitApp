8a04abd1e90317c31d3d9d79dfb374234326776b
Anders Hansen-Haug <anha1607@student.miun.se>
2018-05-03 10:54:24 +0200

    refreshed

---
+++ app/src/main/java/com/gustav/sufitapp/DBConnection.kt
@@ -0,0 +1,14 @@
+package com.gustav.sufitapp
+
+import android.os.AsyncTask;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnection {
+
+public
+
+}
